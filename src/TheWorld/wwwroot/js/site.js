c1b00da4bb5f7d12ea430ffd0f72044d1dbea78f
Chad Techtmann <ctechtmann@gmail.com>
2016-06-18 14:09:42 -0500

    Commit

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+//sie.js
+
+(function () {
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+})();
+
+
